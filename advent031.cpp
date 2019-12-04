78340c184fd9d5119284b70832c8ea48757348d7
paulo404 <paulobrossard@gmail.com>
2019-12-04 16:18:22 -0300

    Update advent031.cpp

--- advent031.cpp
+++ advent031.cpp
@@ -3,20 +3,16 @@
 #include <string>
 #include <sstream>
 #include <climits>
-
-
-// TODO trocar de mat pra  map
-#define MAXHEIGHT 1000
-#define MAXWIDTH 1000
+#include <map>
 
 using namespace std;
 
 int main() {
-	int m[MAXHEIGHT][MAXWIDTH];
+	map<pair<int,int>,int> m;
 	vector<pair<int,int>> collision;
 
 	// central port is at 1,1
-	int curx, cury = 1;
+	int curx=1, cury=1;
 
     // read first line
 	string s; 
@@ -24,58 +20,67 @@ int main() {
 		stringstream ss(s);
 		string dir;
 		ss >> dir;
-		cout << "in: " << s << endl;
+		//cout << "in: " << s << endl;
 		
 		int i=0,j=0,rep=0;
-		
-        //cout << i << " " << j << " " << rep << " ";
-        
-		if (dir == "s")	break;
+		if (dir == "S")	break;
 
-		if (dir == "D")	i=1; 
-		if (dir == "U")	i=-1; 
+		if (dir == "D")	i=-1; 
+		if (dir == "U")	i=1; 
 		if (dir == "R")	j=1; 
 		if (dir == "L")	j=-1; 
 
 		ss >> rep;
-		//cout << i << " " << j << " " << rep << endl;
+		
+		//check deu ruim
+		if (i*j)    return 1;
 		
 		// draw line
 		for (int k=0; k < rep; k++) {
-		    cout << "k " << k << endl;
-			m[cury+=i][curx+=j] = 1;
-			cout << "m[" << cury << "][" << curx << "]" << endl;
+		    m[make_pair(cury+=i,curx+=j)] = 1;
+		    //cout << cury << " " << curx << " " << k << " " << endl;
 		}
 		    
 	}
-	cout << "1";
+	//cout << "1";
 	
+	//back to central point
+	curx=1, cury=1;
+	cout << "-------------------------------------" << endl;
     // read 2nd line
 	while (getline(cin, s))  {
 		stringstream ss(s);
 		string dir;
-		cin >> dir;
+		ss >> dir;
 		
-		int i,j,rep = 0;
+		int i=0,j=0,rep=0;
 
-		if (dir == "s")	break;
-
-		if (dir == "D")	i=1; 
-		if (dir == "U")	i=-1; 
+		if (dir == "D")	i=-1; 
+		if (dir == "U")	i=1; 
 		if (dir == "R")	j=1; 
 		if (dir == "L")	j=-1; 
 
 		ss >> rep;
 		
+		//check deu ruim
+		if (i*j)    return 1;
+		
 		// mark collisions
-		for (int k=0; k < rep; k++)
-			if (m[cury+=i][curx+=j])	collision.push_back(make_pair(cury,curx));
+		for (int k=0; k < rep; k++) {
+		    pair<int,int> p = make_pair(cury+=i,curx+=j);
+		    if (m[p])	
+			    collision.push_back(p);
+		    //cout << cury << " " << curx << " " << k << " " << endl;
+		}
+			
 	}
 	
 	// find closest collsion
 	int mindiff = INT_MAX;
  	for (auto p : collision) {
-		int diff = p.first - 1 + p.second - 1;
+		int diff = abs(p.first) + abs(p.second) - 2;
+		cout << p.first << " " << p.second << endl;
+		//cout << diff << endl;
 		mindiff = diff < mindiff ? diff : mindiff;
  	}
 	cout << mindiff << endl;
